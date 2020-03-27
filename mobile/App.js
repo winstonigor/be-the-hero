f753a8bd7e2677e037b26049db0a4e2780e2937c
Winston Igor <winstonigor14@gmail.com>
2020-03-27 01:11:12 -0300

    Aplicação Mobile

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+import Routes from './src/routes';
+import { View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
